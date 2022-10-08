e9abd6393065313f7ae06d3b00e39ec1bfb04b8a
Paulo Guilherme <paulo.guilherme.arruda1@gmail.com>
2022-10-08 14:08:03 -0300

    fix s3config

--- src/Configs3/s3Config.ts
+++ src/Configs3/s3Config.ts
@@ -1,8 +1,8 @@
 import { S3ClientConfig } from "@aws-sdk/client-s3";
 
 export const s3Config: S3ClientConfig = {
-  credentials: {
+/*  credentials: {
   ,
-  },
+  }, */
   region: "sa-east-1",
 };
